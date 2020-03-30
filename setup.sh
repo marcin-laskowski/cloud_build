e58a1a2d15144484a6ddc37d370c1f6e26812b5c
mlaskowski17 <marcin.ladesign@gmail.com>
2020-03-31 00:06:57 +0200

    deploy

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@
 source config
 export $(cut -d= -f1 config)
 
-# create bucket for config / temp files storage
-gsutil mb -p $PROJECT_ID gs://$BUCKET
-
 
 gcloud projects add-iam-policy-binding developer-269501 \
     --member=serviceAccount:383252814451@cloudbuild.gserviceaccount.com \
