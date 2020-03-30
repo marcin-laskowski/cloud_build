440c9205aca81eae089510d98de8bfcc641ba6e7
mlaskowski17 <marcin.ladesign@gmail.com>
2020-03-31 00:55:34 +0200

    deploy

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ source config
 export $(cut -d= -f1 config)
 
 
-gcloud projects add-iam-policy-binding developer-269501 \
-    --member=serviceAccount:383252814451@cloudbuild.gserviceaccount.com \
-    --role=roles/container.developer
+# gcloud projects add-iam-policy-binding developer-269501 \
+#     --member=serviceAccount:383252814451@cloudbuild.gserviceaccount.com \
+#     --role=roles/container.developer
+
+
+cloud-build-local --config=cloudbuild.yaml \
+     --push=false \
+     --bind-mount-source=true/false \
+     .
